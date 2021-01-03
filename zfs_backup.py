8d988efd19b9e4700643e56f0ae883dedfecc8b0
elurex <elurex@gmail.com>
2021-01-03 19:45:47 +0800

    Update zfs_backup.py

--- zfs_backup.py
+++ zfs_backup.py
@@ -11,14 +11,13 @@ shutdown=config['config']['shutdown']
 today = datetime.now().strftime("%Y-%m-%d-%H%M%S")
 
 for i in pools:
-    
     getsnapshots = subprocess.Popen("zfs list -t snapshot {}|grep {}".format(i,i),shell=True, stdout=subprocess.PIPE )
     snapshots = getsnapshots.communicate()[0].decode("utf-8").strip().splitlines()
     count = len(snapshots)
-    
+
     if(count > 0):
         previous_snapshot = snapshots[count-1].split(" ")[0]
-    
+
     #create today snapshot
     subprocess.run("zfs snapshot {}@{}".format(i,today), shell=True)
     count = count + 1
@@ -26,7 +25,7 @@ for i in pools:
     #check if there is previous snapshot
     if(count > 1):
         checkremote = subprocess.Popen("ssh {} 'zfs list -t snapshot | grep {}'".format(remote_address,previous_snapshot),shell=True, stdout=subprocess.PIPE )
-     
+
         if(i == checkremote.communicate()[0].decode("utf-8").strip().split('@')[0]):
             #send incremental snapshot
             subprocess.run("zfs send -i {} {}@{} | ssh {} zfs recv {}".format(previous_snapshot,i,today,remote_address,i),shell=True)
