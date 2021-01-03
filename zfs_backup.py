d60cea4e948d2477ab02df29c127375e6eed9036
elurex <elurex@gmail.com>
2021-01-03 19:18:34 +0800

    Update zfs_backup.py

--- zfs_backup.py
+++ zfs_backup.py
@@ -13,19 +13,28 @@ yesterday = date.today() - timedelta(1)
 daytoremove = date.today() - timedelta(retention)
 
 for i in pools:
+    
+    getsnapshots = subprocess.Popen("zfs list -t snapshot {}|grep {}".format(i,i),shell=True, stdout=subprocess.PIPE )
+    snapshots = getsnapshots.communicate()[0].decode("utf-8").strip().splitlines()
+    count = len(snapshots)
+    previous_snapshot = snapshots[count-1].split(" ")[0]
+    
     #create today snapshot
     subprocess.run("zfs snapshot {}@{}".format(i,today), shell=True)
+    count = count + 1
 
     #check if there is yesterday snapshot
-    checksnap = subprocess.Popen("zfs list -t snapshot | grep {}@{}".format(i,yesterday),shell=True, stdout=subprocess.PIPE )
-    checkremote = subprocess.Popen("ssh {} 'zfs list -t snapshot | grep {}@{}'".format(remote_address,i,yesterday),shell=True, stdout=subprocess.PIPE )
+    checksnap = subprocess.Popen("zfs list -t snapshot | grep {}".format(i,previous_snapshot),shell=True, stdout=subprocess.PIPE )
+    checkremote = subprocess.Popen("ssh {} 'zfs list -t snapshot | grep {}'".format(remote_address,previous_snapshot),shell=True, stdout=subprocess.PIPE )
 
     if(i ==  checksnap.communicate()[0].decode("utf-8").strip().split('@')[0] and i == checkremote.communicate()[0].decode("utf-8").strip().split('@')[0]):
         #send incremental snapshot
-        subprocess.run("zfs send -i {}@{} {}@{} | ssh {} zfs recv {}".format(i,yesterday,i,today,remote_address,i),shell=True)
+        subprocess.run("zfs send -i {} {}@{} | ssh {} zfs recv {}".format(previous_snapshot,i,today,remote_address,i),shell=True)
         #remove old snapshot
-        subprocess.run("zfs destroy {}@{}".format(i,daytoremove),shell=True)
-        subprocess.run("ssh {} zfs destroy {}@{}".format(remote_address,i,daytoremove),shell=True)
+        if(count - retention > 0):
+            for i in range(count - retention):
+                subprocess.run("zfs destroy {}".format(snapshots[i].split(" ")[0]),shell=True)
+                subprocess.run("ssh {} zfs destroy {}".format(remote_address,snapshots[i].split(" ")[0]),shell=True)
     else:
         #send complete snapshot since there is no increments
         print("send complete snapshot")
